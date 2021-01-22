bed01de08b11abcbe22b217956fd19b1f00aac72
Mahmoud Elzoka <mahmoudelzoka232@gmail.com>
2021-01-22 11:42:38 +0200

    change app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          changed in the feature branch
         </a>
       </header>
     </div>
